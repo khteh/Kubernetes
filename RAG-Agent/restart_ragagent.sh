08207f8d9a202f81c96bf17d452c727906bfcd82
Kok How, Teh <funcoolgeek@gmail.com>
2025-04-25 10:47:21 +0800

    Skip deleting Service in RAG agent restart

--- RAG-Agent/restart_ragagent.sh
+++ RAG-Agent/restart_ragagent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kubectl scale sts ragagent --replicas=0
 kubectl delete statefulset ragagent --ignore-not-found=true
-kubectl delete svc svc-{ragagent,ragagent-nodeport} --ignore-not-found=true
+#kubectl delete svc svc-{ragagent,ragagent-nodeport} --ignore-not-found=true
 kubectl delete secret gcloud-secret ragagent-secret --ignore-not-found=true
 kubectl delete cm elasticsearch-template hypercorn-config ragagent ragagent-fluentd-config --ignore-not-found=true
 cp ~/.config/gcloud/application_default_credentials.json /tmp
