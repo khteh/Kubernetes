a763f3c3f3ef3443faa57df4a198661c3157c922
Kok How, Teh <funcoolgeek@gmail.com>
2026-05-26 14:21:15 +0800

    Move RabbitMQ apps into its folder

--- RabbitMQ/restart_rabbitmqsubscriber.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#./restart_env.sh
-kubectl scale sts kern-subscriber --replicas=0
-kubectl scale sts critical-subscriber --replicas=0
-kubectl delete secret rabbitmq-secret --ignore-not-found=true
-kubectl delete statefulset kern-subscriber critical-subscriber
-kubectl delete svc svc-kern-subscriber svc-critical-subscriber
-kubectl apply -f appsettings.Production.yml,rabbitmq_secret.yml,svc-rabbitmqsubscriber.yml,rabbitmq-kern-subscriber.yml,rabbitmq-critical-subscriber.yml
