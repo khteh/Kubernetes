24575e40db1cb3fdd6313817057a561fa0b68a0c
Kok How, Teh <funcoolgeek@gmail.com>
2026-05-26 20:12:02 +0800

    Add fluentd sidecar to RabbitMQ pubsub sts

--- RabbitMQ/PubSub.Net/restart_rabbitmqpublisher.sh
+++ RabbitMQ/PubSub.Net/restart_rabbitmqpublisher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kubectl delete job rabbitmq-publisher-job --ignore-not-found=true
-kubectl apply -f appsettings.Publisher.Production.yml,rabbitmq-publisher.yml
+kubectl apply -f appsettings.Publisher.Production.yml,publisher_fluentd_config.yml,rabbitmq-publisher.yml
--- RabbitMQ/PubSub.Net/restart_rabbitmqsubscriber.sh
+++ RabbitMQ/PubSub.Net/restart_rabbitmqsubscriber.sh
@@ -5,4 +5,4 @@ kubectl scale sts critical-subscriber --replicas=0
 kubectl delete statefulset kern-subscriber critical-subscriber
 #kubectl delete svc svc-kern-subscriber svc-critical-subscriber
 #kubectl apply -f appsettings.Production.yml,svc-rabbitmqsubscriber.yml,rabbitmq-kern-subscriber.yml,rabbitmq-critical-subscriber.yml
-kubectl apply -f appsettings.CriticalSubscriber.Production.yml,appsettings.KernSubscriber.Production.yml,rabbitmq-kern-subscriber.yml,rabbitmq-critical-subscriber.yml
+kubectl apply -f appsettings.CriticalSubscriber.Production.yml,appsettings.KernSubscriber.Production.yml,subscriber_fluentd_config.yml,rabbitmq-kern-subscriber.yml,rabbitmq-critical-subscriber.yml
