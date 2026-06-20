e337b0a87bc2b254b7de8d161d9af4631e718a21
Kok How, Teh <funcoolgeek@gmail.com>
2026-06-20 11:00:07 +0800

    Delete postgresql-secret in restart script

--- PostgreSQL/restart_postgresql.sh
+++ PostgreSQL/restart_postgresql.sh
@@ -3,6 +3,6 @@ kubectl scale sts postgresql --replicas=0
 kubectl delete svc svc-{postgresql,postgresql-nodeport}
 kubectl delete configmap postgresql-initdb --ignore-not-found=true
 kubectl delete statefulset postgresql --ignore-not-found=true
-#kubectl delete secret postgresql-secret --ignore-not-found=true
+kubectl delete secret postgresql-secret --ignore-not-found=true
 kubectl delete pvc -l app=postgresql
 #kubectl apply -f svc-postgresql.yml,postgresql_secret.yml,postgresql-initdb.yml,postgresql.yml
