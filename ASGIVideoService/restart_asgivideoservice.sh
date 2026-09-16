852db9272686a2d671fa2ca1d9de4f637e923f17
Kok How, Teh <funcoolgeek@gmail.com>
2026-09-16 19:22:59 +0800

    Add ASGIVideoService helm chart

--- ASGIVideoService/restart_asgivideoservice.sh
+++ ASGIVideoService/restart_asgivideoservice.sh
@@ -3,4 +3,4 @@ kubectl scale sts asgivideoservice --replicas=0
 kubectl delete statefulset asgivideoservice --ignore-not-found=true
 #kubectl delete svc svc-{asgivideoservice,asgivideoservice-nodeport} --ignore-not-found=true
 kubectl delete cm elasticsearch-template hypercorn-config asgivideoservice asgivideoservice-fluentd-config --ignore-not-found=true
-kubectl apply -f ../elasticsearch_template.yml,hypercorn_config.yml,asgivideoservice_config.yml,fluentd_config.yml,svc-asgivideoservice.yml,asgivideoservice.yml
+#kubectl apply -f ../elasticsearch_template.yml,hypercorn_config.yml,asgivideoservice_config.yml,fluentd_config.yml,svc-asgivideoservice.yml,asgivideoservice.yml
